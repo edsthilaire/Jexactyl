a27497d8926a9f455d945ff17c1ee8404b66f236
edsthilaire <167595326+edsthilaire@users.noreply.github.com>
2025-07-17 21:25:59 +0000

    Primer commit

---
+++ game.js
@@ -0,0 +1,6 @@
+let puntos = 0;
+
+function aumentarPuntos() {
+  puntos++;
+  document.getElementById("contador").innerText = "Puntos: " + puntos;
+}
--- resources/scripts/components/elements/Label.tsx
+++
@@ -1,9 +0,0 @@
-import tw from 'twin.macro';
-import styled from 'styled-components/macro';
-
-const Label = styled.label<{ isLight?: boolean }>`
-    ${tw`block text-sm font-medium text-neutral-200 font-semibold mb-1 sm:mb-2`};
-    ${(props) => props.isLight && tw`text-gray-400`};
-`;
-
-export default Label;
